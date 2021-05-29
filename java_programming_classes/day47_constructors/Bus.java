456c6b35c742edf335b2deaa1c9c3d981c5eb91a
Jamilya Petsenyuk <jamilya.petsenyuk@gmail.com>
2021-05-29 13:39:49 -0700

    added noted about static into BusObject.java AND created new package inside package

--- java_programming_classes/day47_constructors/Bus.java
+++ java_programming_classes/day47_constructors/Bus.java
@@ -7,6 +7,10 @@ public class Bus {
     public String toString(){
         return driver.getName() + " | "+engine.getCylinders();
     }
+    //static method -> method can be called using the classname. No need for object to be called
+    //static variable -> shared variable, only single copy of the variable exist. And can be used without creating object of the class.
+    //static block -> block of code, that runs ONCE, before anything else when class is loaded memory.
+    //static inner class -> In Java, we can create a class INSIDE another.
 
 
 }
